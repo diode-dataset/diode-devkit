21b7761272d455b27138997314861e7c666c36b4
Haochen Wang <wanghaochen@uchicago.edu>
2019-08-01 22:07:14 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# DIODE: A Dense Indoor and Outdoor DEpth Dataset
+
+
+DIODE (Dense Indoor/Outdoor DEpth) is a dataset that contains diverse high-resolution color images with accurate, dense, and far-range depth measurements. DIODE is the first public dataset to include **RGBD images of indoor and outdoor scenes obtained with one sensor suite**.
+
+Refer to our [homepage](https://diode-dataset.org) and [technical report](https://arxiv.org/abs/1908.00463) for more details.
+
+### Dataset Download
+- [Training set](http://diode-dataset.s3.amazonaws.com/train.tar.gz), 81GB
+- [Validation set](http://diode-dataset.s3.amazonaws.com/val.tar.gz), 2.6 GB
+
+### Dataset Layout
+DIODE data is organized hierarchically.
+![Layout](dataset_layout.png)
+
+### File Naming Conventions
+We have released RGB images, depth maps and depth validity masks:
+
+  RGB images (`*.png`): RGB images with a resolution of 1024 × 768.
+
+  Depth maps (`*_depth.npy`): depth ground truth with the same resolution as the images.
+
+  Depth validity masks (`*_depth_mask.npy`): validity masks where 1 denotes valid sensor return and 0 otherwise.
+
+### Devkit
+This development toolkit contains
+1. A json file that enumerates the data in DIODE. The layout of this file is explained in diode.py. It serves as the single point of reference during dataloading.
+2. A sample pytorch data loading module.
+3. A jupyter-notebook demo showcasing data loading, metadata querying and depth visualization.
+
+
+## Bibtex
+```
+@article{diode_dataset,
+    title={{DIODE}: {A} {D}ense {I}ndoor and {O}utdoor {DE}pth {D}ataset},
+    author={Igor Vasiljevic and Nick Kolkin and Shanyi Zhang and Ruotian Luo and 
+            Haochen Wang and Falcon Z. Dai and Andrea F. Daniele and Mohammadreza Mostajabi and 
+            Steven Basart and Matthew R. Walter and Gregory Shakhnarovich},
+    year = {2019}
+    journal={CoRR},
+    volume={abs/1908.00463},
+    year = {2019},
+    url={http://arxiv.org/abs/1908.00463}
+}
+```
+## Contact
+If you have any questions, please contact us at [diode.dataset@gmail.com](diode.dataset@gmail.com).
---
+++ diode.py
@@ -0,0 +1,106 @@
+import os.path as osp
+from itertools import chain
+import json
+
+from torch.utils.data import Dataset
+import numpy as np
+from PIL import Image
+
+import matplotlib.pyplot as plt
+
+'''
+The json metadata for DIODE is laid out as follows:
+train:
+    outdoor:
+        scene_000xx:
+            scan_00yyy:
+                - 000xx_00yyy_indoors_300_010
+                - 000xx_00yyy_indoors_300_020
+                - 000xx_00yyy_indoors_300_030
+        scene_000kk:
+            _analogous_
+val:
+    _analogous_
+test:
+    _analogous_
+'''
+
+_VALID_SPLITS = ('train', 'val', 'test')
+_VALID_SCENE_TYPES = ('indoors', 'outdoor')
+
+
+def check_and_tuplize_tokens(tokens, valid_tokens):
+    if not isinstance(tokens, (tuple, list)):
+        tokens = (tokens, )
+    for split in tokens:
+        assert split in valid_tokens
+    return tokens
+
+
+def enumerate_paths(src):
+    '''flatten out a nested dictionary into an iterable
+    DIODE metadata is a nested dictionary;
+    One could easily query a particular scene and scan, but sequentially
+    enumerating files in a nested dictionary is troublesome. This function
+    recursively traces out and aggregates the leaves of a tree.
+    '''
+    if isinstance(src, list):
+        return src
+    elif isinstance(src, dict):
+        acc = []
+        for k, v in src.items():
+            _sub_paths = enumerate_paths(v)
+            _sub_paths = list(map(lambda x: osp.join(k, x), _sub_paths))
+            acc.append(_sub_paths)
+        return list(chain.from_iterable(acc))
+    else:
+        raise ValueError('do not accept data type {}'.format(type(src)))
+
+
+def plot_depth_map(dm, validity_mask):
+    validity_mask = validity_mask > 0
+    MIN_DEPTH = 0.5
+    MAX_DEPTH = min(300, np.percentile(dm, 99))
+    dm = np.clip(dm, MIN_DEPTH, MAX_DEPTH)
+    dm = np.log(dm, where=validity_mask)
+
+    dm = np.ma.masked_where(~validity_mask, dm)
+
+    cmap = plt.cm.jet
+    cmap.set_bad(color='black')
+    plt.imshow(dm, cmap=cmap, vmax=np.log(MAX_DEPTH))
+
+
+class DIODE(Dataset):
+    def __init__(self, meta_fname, data_root, splits, scene_types):
+        self.data_root = data_root
+        self.splits = check_and_tuplize_tokens(
+            splits, _VALID_SPLITS
+        )
+        self.scene_types = check_and_tuplize_tokens(
+            scene_types, _VALID_SCENE_TYPES
+        )
+        with open(meta_fname, 'r') as f:
+            self.meta = json.load(f)
+
+        imgs = []
+        for split in self.splits:
+            for scene_type in self.scene_types:
+                _curr = enumerate_paths(self.meta[split][scene_type])
+                _curr = map(lambda x: osp.join(split, scene_type, x), _curr)
+                imgs.extend(list(_curr))
+        self.imgs = imgs
+
+    def __len__(self):
+        return len(self.imgs)
+
+    def __getitem__(self, index):
+        im = self.imgs[index]
+        im_fname = osp.join(self.data_root, '{}.png'.format(im))
+        de_fname = osp.join(self.data_root, '{}_depth.npy'.format(im))
+        de_mask_fname = osp.join(self.data_root, '{}_depth_mask.npy'.format(im))
+
+        im = np.array(Image.open(osp.join(self.data_root, im_fname)))
+        de = np.load(de_fname).squeeze()
+        de_mask = np.load(de_mask_fname)
+        return im, de, de_mask
